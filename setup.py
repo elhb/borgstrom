d1772f914c5db9f0bdf271a685f19dc3308bfeff
Erik Borgström <erikborgstrom@130-229-14-56-dhcp.wlan.ki.se>
2013-01-16 16:12:18 +0100

    First commit.

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.1'
+
+setup(name='borgstrom',
+      version=version,
+      description="For the pyhton course @ scilifelab.",
+      long_description="""\
+NA""",
+      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='NA',
+      author='erik',
+      author_email='erik.borgstrom@scilifelab.se',
+      url='NA',
+      license='NA',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=True,
+      install_requires=[
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
