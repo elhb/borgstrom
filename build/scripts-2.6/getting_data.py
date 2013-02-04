bd619b43ebce7453eb54ab43a27f917cf8cc32cc
Erik Borgström <erik.borgstrom@scilifelab.se>
2013-02-05 00:27:29 +0100

    cleanup and .gitignore

--- build/scripts-2.6/getting_data.py
+++
@@ -1,8 +0,0 @@
-#!/Users/erikborgstrom/.virtualenvs/py2.7/bin/python
-
-from borgstrom.session2 import *
-
-url = 'http://www.grandcentral.org/developers/data/nyct/nyct_ene.xml'
-data = getServerData(url)
-reasons = getReasons(data)
-print getFraction(reasons,'REPAIR')
