7e2d735e6a51d3f07f12d12b439b2d5054b1d355
Erik Borgström <erik.borgstrom@scilifelab.se>
2013-02-27 16:43:53 +0100

    profiling getting_data.py

--- scripts/getting_data.py
+++ scripts/getting_data.py
@@ -1,8 +1,11 @@
 #! /usr/bin/env python2.7
 
 from borgstrom.session2 import *
+import sys
 
-url = 'http://www.grandcentral.org/developers/data/nyct/nyct_ene.xml'
-data = getServerData(url)
-reasons = getReasons(data)
-print getFraction(reasons,'REPAIR')
+#@profile
+def solve():
+	url = 'http://www.grandcentral.org/developers/data/nyct/nyct_ene.xml'
+	data = getServerData(url)
+	reasons = getReasons(data)
+	print getFraction(reasons,'REPAIR')
