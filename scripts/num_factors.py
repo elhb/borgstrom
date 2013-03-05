e18233b1e41f38250edd35d299aee8264857a8a7
Erik Borgström <erik.borgstrom@scilifelab.se>
2013-03-05 19:43:53 +0100

    trying to get the IPython.parallel to work

--- scripts/num_factors.py
+++ scripts/num_factors.py
@@ -4,30 +4,67 @@ def main():
 	indata = check_indata()
 
 	lowest_number = 2
-	highest_number = 500000
+	highest_number = 1000#500000
 
-	# single process
-	timer = Timer(verbose=True)
-	counter = Counter()
-	if indata.mode == 's':
+	if indata.mode == 's' or 'all':
+		timer = Timer(verbose=True)
+		counter = Counter()
 		with timer:
 			for i in xrange(lowest_number,highest_number+1): counter.add(factorize(i))
 			print 'Result from single process:      ', counter, '\t',
-	if indata.mode == 'm':
+	if indata.mode == 'm' or 'all':
+		timer = Timer(verbose=True)
+		counter = Counter()
 		import multiprocessing
 		with timer:
 			WorkerPool = multiprocessing.Pool(2)
 			results = WorkerPool.imap_unordered(factorize,xrange(lowest_number,highest_number+1),chunksize=10)
 			for factors in results: counter.add(factors)
 			print 'Result from multiprocessing.Pool:', counter, '\t',
+	if indata.mode == 'p' or 'all':
+		timer = Timer(verbose=True)
+		counter = Counter()
+		with timer:
+			from IPython.parallel import Client
+			cli = Client()
+			dview = cli[:]
+#			print zip(cli.ids,dview.apply_sync(get_pid))
+	
+			@dview.parallel(block=True)
+			def factorizeIPP(n): # function from Valentin
+				if n < 2:
+					return []
+				factors = []
+				p = 2
+				while True:
+					if n == 1:
+						return factors
+					r = n % p
+					if r == 0:
+						factors.append(p)
+						n = n / p
+					elif p * p >= n:
+						factors.append(n)
+						return factors
+					elif p > 2:
+						p += 2
+					else:
+						p += 1
+	
+			for factors in factorizeIPP.map(xrange(lowest_number,highest_number+1)):
+				counter.add(factors)
+			print 'Result from IPython.parallel:    ', counter, '\t',
+
+def get_pid(): # just for testing
+    import os
+    return os.getpid()
 
 def check_indata():
 	import argparse
-	import sys
 	argparser = argparse.ArgumentParser(description='Script for counting uniqe factors.',formatter_class=argparse.RawTextHelpFormatter,)
-	argparser.add_argument('-mode',dest='mode',metavar='X',type=str,required=True,default='.',help='s m or p (default is s).')
+	argparser.add_argument('-mode',dest='mode',metavar='X',type=str,required=True,default='s',help='s, m, p or all (default is s).')
 	indata = argparser.parse_args()
-	if indata.mode not in ['s','m','p']: raise ValueError, ' mode has to be s m or p!!'
+	if indata.mode not in ['s','m','p', 'all']: raise ValueError, ' mode has to be s m, p or all!!'
 	return indata
 
 def factorize(n): # function from Valentin
