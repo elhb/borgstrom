fd58c573e9a30b8aaa5a1b45e4f9c1c0126df576
Erik Borgström <erik.borgstrom@scilifelab.se>
2013-03-05 17:33:05 +0100

    started multiprocessing stuff

--- scripts/getting_data.py
+++ scripts/getting_data.py
@@ -3,9 +3,13 @@
 from borgstrom.session2 import *
 import sys
 
-#@profile
+print 'test'
+
+@profile
 def solve():
 	url = 'http://www.grandcentral.org/developers/data/nyct/nyct_ene.xml'
 	data = getServerData(url)
 	reasons = getReasons(data)
 	print getFraction(reasons,'REPAIR')
+
+solve()
